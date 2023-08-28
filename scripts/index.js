20ae67366972e8704655108c2e5d6b2ff6acf029
julian23alvarado <julian.d.alvarado23@gmail.com>
2023-08-27 22:26:06 -0500

    inital commit

---
+++ scripts/index.js
@@ -0,0 +1,12 @@
+const btnAboutUs = document.getElementById("about-us")
+const btnMenu = document.getElementById("menu")
+
+btnAboutUs.addEventListener("click", () => {
+
+    window.location.href = "http://127.0.0.1:5500/index2.html";
+})
+
+btnMenu.addEventListener("click", () => {
+
+    window.location.href = "http://127.0.0.1:5500/menu.html";
+})
