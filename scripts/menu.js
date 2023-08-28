3696a35321745c77bbe7e4a1e8684d5a83426aad
julian23alvarado <julian.d.alvarado23@gmail.com>
2023-08-28 02:17:10 -0500

    aniade y elimina correctamente, falta calcular bien

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,19 +1,104 @@
-const listElements = document.querySelectorAll(".product-click")
+let productsOnTheCart = [];
+let totalPrice = 10;
+
+const listElements = document.querySelectorAll(".product-click");
+const containerBuyCart = document.querySelector(".products-to-buy");
+const finalPrice = document.querySelector(".final-price");
+const allContainerProducts = document.querySelector(".show");
 
 listElements.forEach((listElement) => {
-    listElement.addEventListener("click", () => {
+  listElement.addEventListener("click", () => {
+    listElement.classList.toggle("hello");
 
-        listElement.classList.toggle("hello")
+    let height = 0;
+    let menu = listElement.nextElementSibling;
+    console.log(menu);
+    if (menu.clientHeight == "0") {
+      height = menu.scrollHeight;
+    }
 
-        let height = 0
-        let menu = listElement.nextElementSibling;
-        console.log(menu);
-        if(menu.clientHeight == "0"){
+    menu.style.height = `${height}px`;
+  });
+});
 
-            height = menu.scrollHeight;
-        }
+const deleteProduct = (e) => {
+  if (e.target.classList.contains("delete-product")) {
+    const deleteId = e.target.getAttribute("data-id");
+    productsOnTheCart = productsOnTheCart.filter(
+      (product) => product.id !== deleteId
+    );
+  }
+  addToSummary();
+};
 
-        menu.style.height = `${height}px`
-    })
+const clearHtml = () => {
+  containerBuyCart.innerHTML = "";
+};
 
-})
+const addToSummary = () => {
+  clearHtml();
+  productsOnTheCart.forEach((product) => {
+    const { image, title, price, amount, id } = product;
+    const productCard = document.createElement("article");
+    productCard.classList.add("product-inside");
+    productCard.innerHTML = `
+        <img src=${image} class="product-img"/>
+            <div class="info-container">
+                <div class="about">
+                    <h4 class="title">${title}</h4>
+                    <p class="price">${price}</p>
+                    <h5>Amount: ${amount}</h5>
+                </div>
+            </div>
+            <span class="delete-product" data-id="${id}">X</span>
+        `;
+
+    containerBuyCart.appendChild(productCard);
+
+    finalPrice.innerHTML = totalPrice;
+  });
+};
+
+const readContent = (product) => {
+  const infoProduct = {
+    image: product.querySelector("article img").src,
+    title: product.querySelector(".title").textContent,
+    price: product.querySelector(".price").textContent,
+    id: product.querySelector(".add-to-cart").getAttribute("data-id"),
+    amount: 1,
+  };
+
+  console.log(totalPrice, "Xd");
+  totalPrice = totalPrice;
+
+  const exist = productsOnTheCart.some(
+    (product) => product.id === infoProduct.id
+  );
+  if (exist) {
+    const pro = productsOnTheCart.map((product) => {
+      if (product.id === infoProduct.id) {
+        product.amount++;
+      }
+      return product;
+    });
+    productsOnTheCart = [...pro];
+  } else {
+    productsOnTheCart = [...productsOnTheCart, infoProduct];
+  }
+
+  addToSummary();
+};
+
+const addProduct = (e) => {
+  e.preventDefault();
+  if (e.target.classList.contains("add-to-cart")) {
+    const selectedProduct = e.target.parentElement;
+    readContent(selectedProduct);
+  }
+};
+
+const loadEventListeners = () => {
+  allContainerProducts.addEventListener("click", addProduct);
+  containerBuyCart.addEventListener("click", deleteProduct);
+};
+loadEventListeners();
